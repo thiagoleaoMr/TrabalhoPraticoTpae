85d7c443ab2260a60475c9623000847d6ab01cc2
thiagoleaoMr <thiagol.alternativo@gmail.com>
2022-08-27 21:53:58 -0300

    first commit

---
+++ src/trabalhopratico/Personagem.java
@@ -0,0 +1,29 @@
+package trabalhopratico;
+
+public abstract class Personagem {
+    private String nome;
+    private String descricao;
+
+    public Personagem(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Personagem:" + nome + descricao;
+    }
+}
+
+//testando github
+//testando de novooosdjl
+//qawocuisjnwaschnioklmqacfs
+/iknascoink
